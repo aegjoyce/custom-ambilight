6376b83bded4c46896c1b71dc57671412b668e14
aegjoyce <47081267+aegjoyce@users.noreply.github.com>
2024-03-27 14:38:57 +0100

    updates

--- custom_components/custom-ambilight/__init__.py
+++ custom_components/custom-ambilight/__init__.py
@@ -1,13 +1,1 @@
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
-
-DOMAIN = "CustomAmbilight"
-
-CONFIG_SCHEMA = vol.Schema(
-    {DOMAIN: cv.schema_with_slug_keys(vol.Any(cv.boolean, cv.string))},
-    extra=vol.ALLOW_EXTRA,
-)
-
-async def async_setup(hass, config):
-    """Set up the Custom Ambilight integration."""
-    return True
+"""Custom Ambilight integration."""
--- custom_components/custom-ambilight/light.py
+++ custom_components/custom-ambilight/light.py
@@ -1,21 +1,52 @@
+from __future__ import annotations
+import logging
+import voluptuous as vol
 import aiohttp
+
+# Import the device class from the component that you want to support
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
     SUPPORT_BRIGHTNESS,
     LightEntity,
+    PLATFORM_SCHEMA
 )
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+# Validation of the user's configuration
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+})
+
+def setup_platform(hass: HomeAssistant, config: ConfigType, add_entities: AddEntitiesCallback, discovery_info: DiscoveryInfoType = None) -> None:
+    """Set up the Custom Ambilight platform."""
+    # Get configuration data
+    host = config[CONF_HOST]
+    username = config[CONF_USERNAME]
+    password = config[CONF_PASSWORD]
+
+    # Create our custom light
+    light = CustomAmbilight('Custom Ambilight', username, password, host)
+
+    # Add entities
+    add_entities([light])
 
 class CustomAmbilight(LightEntity):
     """Representation of a Custom Ambilight."""
 
-    def __init__(self, name, value, username, password, ip_address):
+    def __init__(self, name, username, password, host):
         """Initialize a Custom Ambilight."""
         self._name = name
         self._state = False
         self._brightness = 255
         self._username = username
         self._password = password
-        self._ip_address = ip_address
+        self._host = host
 
     @property
     def name(self):
@@ -46,7 +77,7 @@ class CustomAmbilight(LightEntity):
         """Instruct the light to turn on."""
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                f"https://{self._ip_address}:1926/6/ambilight/power",
+                f"https://{self._host}:1926/6/ambilight/power",
                 auth=aiohttp.BasicAuth(self._username, self._password),
                 data='{"power":"on"}'
             ) as response:
@@ -59,7 +90,7 @@ class CustomAmbilight(LightEntity):
         """Instruct the light to turn off."""
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                f"https://{self._ip_address}:1926/6/ambilight/power",
+                f"https://{self._host}:1926/6/ambilight/power",
                 auth=aiohttp.BasicAuth(self._username, self._password),
                 data='{"power":"off"}'
             ) as response:
@@ -69,7 +100,7 @@ class CustomAmbilight(LightEntity):
     async def async_update(self):
         """Fetch new state data for this light."""
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://{self._ip_address}:1926/6/ambilight/currentconfiguration", auth=aiohttp.BasicAuth(self._username, self._password)) as response:
+            async with session.get(f"https://{self._host}:1926/6/ambilight/currentconfiguration", auth=aiohttp.BasicAuth(self._username, self._password)) as response:
                 if response.status == 200:
                     state_data = await response.json()
                     self._state = state_data["state"] == "on"
