b8af2f99763761bc59803c0ab09d8325aee23c3f
aegjoyce <47081267+aegjoyce@users.noreply.github.com>
2024-11-20 11:38:14 +0000

    fix async_forward_entry_setups

--- custom_components/custom_ambilight/__init__.py
+++ custom_components/custom_ambilight/__init__.py
@@ -49,10 +49,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator.api = api
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    # Forward the entry setup to the platforms
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
